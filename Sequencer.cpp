4923fecc77dae2ef0963f653d5cb6c56e32636b9
Austin <roweaustin@gmail.com>
2013-11-12 20:05:24 -0500

    Update Sequencer.cpp

--- Sequencer.cpp
+++ Sequencer.cpp
@@ -2,21 +2,31 @@
 #include "Sequencer.h"
 
 Sequence::Sequence() {}
-void Sequence::setFuse(int Time) {
+void Sequence::setTime(int Time) {
 _Time = Time;
 }
-void Sequence::start(int Steps) {
+void Sequence::start(int Steps, int Iterations) {
+if (Complete == false) {
 unsigned long TimeCurrent = millis();
+if(Iteration == Iterations && Iterations != 0) {
+Complete = true;
+Iteration = 0;
+} else {
 if (paused == false) {
 if(TimeCurrent - TimeLast >= _Time) {
 TimeLast = TimeCurrent;
 Step++;
 if (Step > Steps) {
 Step = 1;
+Iteration++;
    }
   }
  } else {
  TimePaused = TimeCurrent - TimeLast;
+ }
+ }
+ } else {
+ Step = 0;
  }
 }
 void Sequence::pause() {
@@ -28,6 +38,21 @@ paused = false;
 }
 void Sequence::gotoStep(int StepNum) {
 Step = StepNum;
+}
+void Sequence::reset() {
+Complete = false;
+Step = 1;
+}
+bool Sequence::getComplete() {
+return Complete;
+}
+int Sequence::getIteration() {
+return Iteration;
+}
+int Sequence::getStep() {
+return Step;
+}
+
 }
 int Sequence::getStep() {
 return Step;
